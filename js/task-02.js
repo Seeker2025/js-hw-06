a3015612e716728e11d88a03d08e0f42f517ded1
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2024-11-16 19:57:46 +0200

    go on

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,26 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+const refListTomat = document.querySelector('#ingredients');
+
+// const arr = ingredients.map((itm) => {
+//   // console.log(itm);
+//   const el = document.createElement('li');
+//   el.classList.add('item');
+//   el.textContent = itm;
+//   return refListTomat.appendChild(el);
+//  });
+// console.log(refListTomat);
+
+/////vertion 02
+const arr = ingredients.map((itm) => {
+  return `<li>${ itm }</li>`;
+});
+console.log(refListTomat);
+const str = arr.join('');
+// console.log(str);
+// console.log(typeof (str));
+refListTomat.innerHTML = str;
+console.log(refListTomat);
+
+
