f53408070b161dac109a16b76d09fd8db113fdbc
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2024-11-23 17:12:55 +0200

    go on

--- js/task-07.js
+++ js/task-07.js
@@ -4,7 +4,6 @@ console.log(refFontSizeControl);
 console.log(refSpanText.style.fontSize);
 
 refFontSizeControl.addEventListener('input', sliderFun);
-
 function sliderFun(event) {
     console.log('value', event.target.value);
     refSpanText.style.fontSize = `${event.target.value}px`;
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
-
+const formEl = document.querySelector('.login-form');
+// console.dir(formEl);
+formEl.addEventListener('submit', formFun);
+function formFun(event) {
+    event.preventDefault();
+    const allBigForm = event.target;
+    // console.log(event.currentTarget);
+    const { email, password } = event.currentTarget.elements;
+    //  console.log(email.value);
+    //  console.log(password.value);
+    const dataForm = {
+        email: email.value,
+        password: password.value,
+    };
+    if (email.value === '' || password.value === '') {
+        alert('All fields must be filled in!'); 
+    }
+    allBigForm.reset();
+console.log(dataForm);
+}
+////// well done!
