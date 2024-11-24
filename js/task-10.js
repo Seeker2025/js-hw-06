ab29edb80c3fc39810dd3bdb3225be14bfc34e1e
Igor Yurovsky <85400723+Seeker2025@users.noreply.github.com>
2024-11-24 13:27:30 +0200

    go on

--- js/task-10.js
+++ js/task-10.js
@@ -1,33 +1,57 @@
-// function getRandomHexColor() {
-//   return `#${Math.floor(Math.random() * 16777215)
-//     .toString(16)
-//     .padStart(6, 0)}`;
-// }
-
-const refBoxes = document.getElementById('boxes');
-console.log(refBoxes);
-const  square = 30;
-const toAdd = 10;
-// function createBox(num) {
-  
-//   const boxOne = document.createElement('div');
-// boxOne.classList.add('boxOne');
-// boxOne.style.width = `${square+num}px`;
-// boxOne.style.height = `${square+num}px`;
-// boxOne.style.background = "red";
-
-// refBoxes.append(boxOne);
-// }
-
-
-let num03 = 10;
-function toAdding() {
-  num03 += 10;
-  console.log(num03);
+////// Random color function
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
 
-toAdding()
-toAdding()
-toAdding()
+const refBoxes = document.getElementById('boxes');
+// console.log(refBoxes);
+const butCreate = document.querySelector('button.butCreate');
+// console.log(butCreate);
+const butDestroy = document.querySelector('button.butDestroy');
+// console.log(butDestroy);
+const inNumber = document.querySelector('input.inNumber');
+// console.log(inNumber);
+
+let square = 30;
+let amaunt = 0;
+
+function createBoxes() {
+      for (let i = 0; i < amaunt; i += 1) {
+        boxOneFun();
+        square += 10;
+}
+  square = 30;
+}
+
+function boxOneFun() {
+const boxOne = document.createElement('div');
+boxOne.classList.add('boxOne');
+boxOne.style.width = `${square}px`;
+boxOne.style.height = `${square}px`;
+boxOne.style.background = getRandomHexColor();
+refBoxes.append(boxOne);
+}
+
+function toDestroy() {
+  refBoxes.innerHTML = '';
+  }
+
+butCreate.addEventListener('click', createBoxes);
+butDestroy.addEventListener('click', toDestroy);
+inNumber.addEventListener('input', toInput);
+
+function toInput(event) {
+  // console.log(event.currentTarget.value);
+  amaunt = event.currentTarget.value;
+    
+}
+
+
+
+
+
+
 
 
